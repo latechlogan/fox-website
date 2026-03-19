a684e72b187a789271d9a5cc9dc9ef6a0b072645
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-03-19 15:15:14 -0500

    feat: implement LogoGrid component

---
+++ src/data/logos.ts
@@ -0,0 +1,25 @@
+import raisingCanes from "../assets/raising-canes-logo.png";
+import fastPaceHealth from "../assets/fastpace-logo.png";
+import liveSwitch from "../assets/liveswitch-logo.png";
+
+type Logo = {
+  src: ImageMetadata;
+  alt: string;
+};
+
+export const logos: Logo[] = [
+  {
+    src: raisingCanes,
+    alt: "Raising Cane's logo",
+  },
+  {
+    src: fastPaceHealth,
+    alt: "Fast Pace Healt logo",
+  },
+  {
+    src: liveSwitch,
+    alt: "LiveSwitch logo",
+  },
+];
+
+// export const awards: logo[] = []
