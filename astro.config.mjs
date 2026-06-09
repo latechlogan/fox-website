b1b1ff2abb504ae0eca53d2590f13fc3ebe8d59f
Logan Baugh <loganbaugh@Logans-MacBook-Pro.local>
2026-06-09 10:46:55 -0500

    fix: update build/deploy for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://astronaut.github.io",
+  site: "https://latechlogan.github.io",
   base: "/fox-website",
 });
