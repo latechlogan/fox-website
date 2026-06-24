605456cb046764ba50f420fda0594f609310f39f
Logan Baugh <loganbaugh@Logans-MacBook-Pro.local>
2026-06-24 10:45:50 -0500

    refactor: update astro.config.mjs for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://latechlogan.github.io",
-  base: "/fox-website",
+  // site: "https://latechlogan.github.io",
+  // base: "/fox-website",
 });
